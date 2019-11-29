f5ba23dd992c6e8ebf8bc4e1052ad4e96b4067b3
errich <lahcenerrich@gmail.com>
2019-11-29 12:30:08 -0500

    update the view

--- User/views.py
+++ User/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import User
 from .form import UserForm
 
-def product_list(request):
+def user_list(request):
     users = User.objects.all()
     return render(request, 'User/user_list.html', {'products':users})
 
@@ -12,7 +12,7 @@ def user_new(request):
         if form.is_valid():
             product = form.save(commit=True)
             product.save()
-            return redirect('User_list', pk=product.pk)
+            return redirect('User_list')
     else:
         form = UserForm()
-        return render(request, 'User/product_edit.html', {'form': form})
+        return render(request, 'User/user_edit.html', {'form': form})
